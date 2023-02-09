792608c525f2757278b68d5a94286656b19ad956
Vaishali Ranjan <110801149+vaishaliranjan@users.noreply.github.com>
2023-02-10 00:40:45 +0530

    Create 4x5forloop.cpp

---
+++ DSA/Flow Control, Functions and Loop/4x5forloop.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=0; i<4;i++)
+    {
+        for(int j=1;j<=4;j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
