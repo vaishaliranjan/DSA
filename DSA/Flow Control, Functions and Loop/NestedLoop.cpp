e0302e319957a5478c8dba5d926276b9226ecdb9
Vaishali Ranjan <110801149+vaishaliranjan@users.noreply.github.com>
2023-02-10 00:05:55 +0530

    Create NestedLoop.cpp

---
+++ DSA/Flow Control, Functions and Loop/NestedLoop.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"no. ";
+    cin>>n;
+    for(int i=1; i<=n;i++)
+    {
+        for(int j=1; j<=10; j++)
+        {
+            cout<<i*j<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
